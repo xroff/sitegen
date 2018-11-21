3704da6f422191e694b174a1631f68df78f549cf
Горлов Константин Геннадьевич <gkg@c1r346n05.main.vsu.ru>
2018-11-21 11:45:09 +0300

    Add generate menu block

--- gen.py
+++ gen.py
@@ -1,55 +1,73 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import json
-from os import walk, mkdir
-from os.path import isfile, join
+from os import walk, mkdir, listdir
+from os.path import isfile, isdir, join
 
 srcDir = 'src'
 siteDir = 'site'
 
+# test direxist
+if not isdir(srcDir):
+	exit('{}: source dir not exist'.format(srcDir))
+
+if not isdir(siteDir):
+	exit('{}: site dir not exist'.format(siteDir))
 
 # Init inside vars
 menuList = []
-
-class MenuButton:
-	stitle=''
-	link=''
-	def __init__(self, menuJson):
-		f_menuJson = open(menuJson)
-		data = json.load(f_menuJson)
-		f_menuJson.close()
-		self.title = data['title']
-		self.link = data['link']
-	
+pagesDir = join(srcDir,'pages')
 
 		
-
-with open(srcDir + '/global.json','r') as configFile:
-	globalConf = json.load(configFile)
-	configFile.close()
-
+# read global configuration
+with open(join(srcDir,'global.json'),'r') as configFile:
+	globalConf = json.load(configFile, encoding='utf-8')
 print(globalConf)
+globalConf['templatesDir']    = join(srcDir,globalConf['templatesDir'])
+globalConf['mainTemplate']    = join(globalConf['templatesDir'],globalConf['mainTemplate'])
+globalConf['menuTemplate']    = join(globalConf['templatesDir'],globalConf['menuTemplate'])
+globalConf['contentTemplate'] = join(globalConf['templatesDir'],globalConf['contentTemplate'])
 
-stucture = walk(srcDir)
+# Firstly generate menu list
+pages = listdir(pagesDir)
+pages.sort()
+for i in pages:
+	dirPath = join(pagesDir,i)
+	confFilePath = join(dirPath,'conf.json')
+	if isdir(dirPath) and isfile(confFilePath):
+		with open(confFilePath) as confFile:
+			conf = json.load(confFile,encoding='utf-8')
+			if conf['menu']=='True':
+				conf['path'] = dirPath
+				print('Adding {} {} to menu'.format(conf['title'], conf['link']))
+				menuList.append(conf)
+				
+# Next generate pages
+with open(globalConf['menuTemplate']) as f:
+	menuTemplateStr = f.read()
+with open(globalConf['mainTemplate']) as f:
+	mainTemplateStr = f.read()
+with open(globalConf['contentTemplate']) as f:
+	contentTemplateStr = f.read()
+
+for page in listdir(pagesDir):
+	dirPath = join(pagesDir,page)
+	confFilePath = join(dirPath,'conf.json')
+	if isdir(dirPath) and isfile(confFilePath):
+		with open(confFilePath) as confFile:
+			pageConf = json.load(confFile,encoding='utf-8')
+			#generate menu
+			menuBlock = ''
+			for menuItem in menuList:
+				if (dirPath == menuItem['path']): menuItem['active'] = 'class="active"'
+				else: menuItem['active'] = ''
+				menuBlock += menuTemplateStr.format(**menuItem)
+			print('Menu for {}'.format(conf['title']))
+			print(menuBlock)
+
+			
 
 
 
-for (path, dirs, files) in stucture:
-	dirs.sort()
-	print(dirs)
-	for button in dirs:
-		menuJson = path + '/' + button +'/conf.json'
-		if isfile(menuJson):
-			menuList.append(MenuButton(menuJson))
-	break
 
-print(menuList[0].link)
-
-#for (path, dirs, files) in stucture:
-#	dirs.sort()
-#	print(path, dirs, files)
-#	menuJson = path+'/menu.json'
-#	if isfile(menuJson):
-#		f_menuJson = open(menuJson)
-#		data_f_menuJson = json.load(f_menuJson)
-#		menuList.append(data_f_menuJson['title'])
